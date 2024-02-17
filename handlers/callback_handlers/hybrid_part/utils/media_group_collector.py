ae61a37ee165ee7924a1a4eafb329b81b1665746
Boris Shein <wkatano@mail.ru>
2024-02-17 20:13:30 +0500

    the minimum quality of photos from the user is limited

--- handlers/callback_handlers/hybrid_part/utils/media_group_collector.py
+++ handlers/callback_handlers/hybrid_part/utils/media_group_collector.py
@@ -10,36 +10,25 @@ from aiogram.types import Message, InputMediaPhoto, FSInputFile
 from httpx import ConnectError
 
 from handlers.custom_filters.message_is_photo import MessageIsPhoto
+from handlers.state_handlers.seller_states_handler.load_new_car.hybrid_handlers import input_photo_to_load
+from handlers.utils.delete_message import delete_message
 from states.admin_part_states.catalog_states.advert_parameters_states import AdminAdvertParametersStates
 from states.load_commodity_states import LoadCommodityStates
 from utils.middleware.update_spam_defender.modules.language import LanguageMiddlewareModule
 
 # from utils.photo_utils.insert_watermark import insert_watermark
 
-mediagroups = {}
-user_messages = []
+mediagroups = dict()
+min_side_size = 400
+user_messages = dict()
+media_incorect_flag = dict()
 
 seller_boot_commodity_module = importlib.import_module(
     'handlers.state_handlers.seller_states_handler.load_new_car.get_output_configs')
 
-async def get_unique_dictionaries(mediagroups):
-    unique_mediagroups = {}
 
-    for key, value in mediagroups.items():
-        unique_combinations = set()
-        unique_dicts = []
-
-        for item in value:
-            id_pair = (item.get('id'), item.get('unique_id'))
-
-            if id_pair not in unique_combinations:
-                unique_combinations.add(id_pair)
-                unique_dicts.append(item)
-
-        unique_mediagroups[key] = unique_dicts
-
-    return unique_mediagroups
-async def collect_and_send_mediagroup(message: Message, state: FSMContext, photo_id: int, unique_id: int):
+async def collect_and_send_mediagroup(message: Message, state: FSMContext, photo_id: int, unique_id: int,
+                                      photo_width: int, photo_height: int):
 
     new_car_state_parameters_module = importlib.import_module(
         'handlers.callback_handlers.admin_part.admin_panel_ui.catalog.advert_parameters.advert_parameters__new_state_handlers.new_car_state_parameters_handler')
@@ -50,15 +39,20 @@ async def collect_and_send_mediagroup(message: Message, state: FSMContext, photo
     # if message.from_user.is_bot:
     #
     #     return
+    user_id = message.from_user.id
+    if not user_messages.get(user_id):
+        user_messages[user_id] = []
 
-    try:
-        await message.delete()
-    except:
-        pass
+    if not mediagroups.get(user_id):
+        mediagroups[user_id] = dict()
+    # try:
+    #     await message.delete()
+    # except:
+    #     pass
     ic()
     ic(state)
-
-    if not await state.get_state() in (LoadCommodityStates.photo_verification,
+    state_name = await state.get_state()
+    if not state_name in (LoadCommodityStates.photo_verification,
                              AdminAdvertParametersStates.NewStateStates.await_input_new_car_photos):
 
         return
@@ -66,43 +60,65 @@ async def collect_and_send_mediagroup(message: Message, state: FSMContext, photo
     album_id = message.media_group_id
     ic(album_id)
     if album_id:
-        if album_id in mediagroups:
-            mediagroups[album_id].append({'id': photo_id, 'unique_id': unique_id})
-            user_messages.append(message.message_id)
+        ic(photo_height, photo_width)
+        ic(min_side_size)
+        ic(photo_height < min_side_size, photo_width < min_side_size)
+        ic(any(side < min_side_size for side in (photo_height, photo_width)), state_name, state_name == 'LoadCommodityStates:photo_verification')
+        if any(side < min_side_size for side in (photo_height, photo_width)) and state_name == 'LoadCommodityStates:photo_verification':
+            media_incorect_flag[user_id] = 'size'
+        if album_id in mediagroups.get(user_id):
+            mediagroups[user_id][album_id].append({'id': photo_id, 'unique_id': unique_id})
+            user_messages[user_id].append(message.message_id)
             return
-        mediagroups[album_id] = [{'id': photo_id, 'unique_id': unique_id}]
-        user_messages.append(message.message_id)
+        mediagroups[user_id][album_id] = [{'id': photo_id, 'unique_id': unique_id}]
+        user_messages[user_id].append(message.message_id)
         await asyncio.sleep(2.5)
         # media_group = await insert_watermark(mediagroups[album_id], message.chat.id, message.bot)
+        await delete_message(message, user_messages[user_id])
 
         # new_album = [InputMediaPhoto(media=file_id) for file_id in mediagroups[album_id]]
 
         # await handle_user_input_photos(album_id, message, state)
-
+        ic(mediagroups, user_messages)
         if state:
+
             state_name = await state.get_state()
             ic(state_name)
             #
             if state_name:
                 if state_name in ('LoadCommodityStates:photo_verification', 'AdminAdvertParametersStates.NewStateStates:await_input_new_car_photos'):
-                    if not 5 <= len(mediagroups[album_id]) <= 8:
+                    if not 5 <= len(mediagroups[user_id][album_id]) <= 8:
 
                         photo_filter = MessageIsPhoto()
-                        mediagroups.clear()
-                        user_messages.clear()
+                        mediagroups[user_id].clear()
+                        user_messages[user_id].clear()
+                        media_incorect_flag[user_id] = None
+
                         await photo_filter(message=message, state=state)
                         return
                     match state_name:
                         case 'LoadCommodityStates:photo_verification':
-                            await handle_user_input_photos(album_id, message, state)
+                            memory_storage = await state.get_data()
+                            # media_incorect_flag = memory_storage.get('media_incorect_flag')
+                            ic(media_incorect_flag)
+                            ic()
+                            if media_incorect_flag.get(user_id):
+                                await input_photo_to_load(message, state, incorrect='size')
+                                del media_incorect_flag[user_id]
+                            else:
+                                await handle_user_input_photos(mediagroups[user_id][album_id], message, state)
                         case 'AdminAdvertParametersStates.NewStateStates:await_input_new_car_photos':
                             await new_car_state_parameters_module\
-                                .NewCarStateParameters().callback_handler(message, state, media_photos=mediagroups)
-                    mediagroups.clear()
+                                .NewCarStateParameters().callback_handler(message, state, media_photos=mediagroups[user_id])
+                    mediagroups[user_id].clear()
 
             elif state_name == "MailingStates:uploading_media":
-                return mediagroups
+                result = mediagroups[user_id]
+                mediagroups[user_id].clear()
+                return result
     else:
+        await delete_message(message, user_messages[user_id])
+
         if state:
             state_name = await state.get_state()
             ic(state_name)
@@ -111,13 +127,13 @@ async def collect_and_send_mediagroup(message: Message, state: FSMContext, photo
                 match state_name:
                     case 'LoadCommodityStates:photo_verification':
                         await seller_boot_commodity_module.output_load_config_for_seller(request=message, state=state,
-                                                                                         media_photos=mediagroups)
+                                                                                         media_photos=mediagroups[user_id])
                     case 'AdminAdvertParametersStates.NewStateStates:await_input_new_car_photos':
                         await new_car_state_parameters_module\
-                            .NewCarStateParameters().callback_handler(message, state, media_photos=mediagroups)
+                            .NewCarStateParameters().callback_handler(message, state, media_photos=mediagroups[user_id])
 
 
-async def handle_user_input_photos(album_id, message, state):
+async def handle_user_input_photos(mediagroups, message, state):
     redis_data_module = importlib.import_module('utils.redis_for_language')
     message_editor_module = importlib.import_module('handlers.message_editor')
     Lexicon_module = importlib.import_module('utils.lexicon_utils.Lexicon')
@@ -155,7 +171,7 @@ async def handle_user_input_photos(album_id, message, state):
     #                                                        lexicon_part=Lexicon_module.LEXICON['awaiting_process'],
     #                                                        lexicon_key='')
 
-    tasks = [bot.get_file(mediagroup['id']) for mediagroup in mediagroups[album_id]]
+    tasks = [bot.get_file(mediagroup['id']) for mediagroup in mediagroups]
     files_info = await asyncio.gather(*tasks)
     urls = [f"https://api.telegram.org/file/bot{bot.token}/{file_info.file_path}" for file_info in files_info]
 
@@ -169,7 +185,7 @@ async def handle_user_input_photos(album_id, message, state):
     else:
         with_watermark = False
 
-        medias = [photo['id'] for photo in mediagroups[album_id]]
+        medias = [photo['id'] for photo in mediagroups]
         # from handlers.utils.message_answer_without_callback import send_message_answer
         # Lexicon_module = importlib.import_module('utils.lexicon_utils.Lexicon')
         #
